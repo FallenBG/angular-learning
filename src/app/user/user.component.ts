6d8a573c4de1539c56dbe76156ed8ca0376d416e
Martin <martin@pliant.io>
2024-08-01 06:35:04 +0300

    Part 40

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,18 @@
 import { Component, Input, Output, EventEmitter, output } from '@angular/core';
 
+// type User = { 
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
+
+interface User  {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,15 +22,7 @@ import { Component, Input, Output, EventEmitter, output } from '@angular/core';
 })
 
 export class UserComponent {
-  // @Input({required: true}) id!: string;
-  // @Input({required: true}) avatar!: string;
-  // @Input({required: true}) name!: string;
-
-  @Input({required: true}) user!: { 
-    id: string;
-    avatar: string;
-    name: string;
-  };
+  @Input({required: true}) user!: User;
 
   @Output() select  = new EventEmitter<string>();
   // select = output<string>();
