68b580ae88d69ba0976c987ed7135191cc35408e
Martin <martin@pliant.io>
2024-08-01 07:01:25 +0300

    Part 43 - revert ng nonsence.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from "./tasks/tasks.component";
-import { NgFor, NgIf } from '@angular/common';
 
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserName?: string;
+  selectedUserName?: string = '';
 
   
 
